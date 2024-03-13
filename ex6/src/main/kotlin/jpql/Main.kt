d4199c8266db82ac2b0e101f9ed35bd73b85ab33
kimbackdoo <kdk7121743@naver.com>
2024-03-13 16:10:53 +0900

    feat: fetch join 연습

--- ex6/src/main/kotlin/jpql/Main.kt
+++ ex6/src/main/kotlin/jpql/Main.kt
@@ -5,9 +5,35 @@ import jakarta.persistence.Persistence
 fun main() {
     val entityManagerFactory = Persistence.createEntityManagerFactory("hello")
     val entityManager = entityManagerFactory.createEntityManager()
+    val transaction = entityManager.transaction
+    transaction.begin()
 
-    entityManager.createQuery("select m from Member m join m.team t", Member::class.java)
+    val teamA = Team("teamA")
+    entityManager.persist(teamA)
+
+    val teamB = Team("teamB")
+    entityManager.persist(teamB)
+
+    val member1 = Member("member1", 10, teamA)
+    entityManager.persist(member1)
+
+    val member2 = Member("member2", 20, teamA)
+    entityManager.persist(member2)
+
+    val member3 = Member("member3", 30, teamB)
+    entityManager.persist(member3)
+
+    entityManager.flush()
+    entityManager.clear()
+
+    val members = entityManager.createQuery("select m from Member m join fetch m.team t", Member::class.java)
             .resultList
+
+    for (member in members) {
+        println("member.username = ${member.username}, team.name = ${member.team.name}")
+    }
+
+    transaction.commit()
 
     entityManager.close()
     entityManagerFactory.close()
